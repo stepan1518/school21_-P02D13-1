29875056436a9b7bf88f1f6e3b71100e81fe4cdb
Argonian Maxi <argonima@ga-h2.msk.21-school.ru>
2024-10-12 19:37:47 +0300

    Full project

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,65 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ncurses.h>
 
 #define HEIGHT 25
 #define WIDTH 80
 
-int **init_matrix(int n, int m);
-void init_grid_from_file(int **grid);
-void output_matrix(int **matrix);
+void init_grid_from_file(int grid[][WIDTH]);
+void render_canvas(int grid[][WIDTH]);
+void move_grid(int grid[][WIDTH]);
+int neighbours_count(int grid[][WIDTH], int x, int y);
 
 int main() {
-    int error = 0;
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
 
-    if (error != 1) {
-        int **grid = init_matrix(HEIGHT, WIDTH);
-        init_grid_from_file(grid);
+    int grid[HEIGHT][WIDTH];
+    init_grid_from_file(grid);
 
-        if (error != 1) {
-            output_matrix(grid);
-        }
+    while (1) {
+        render_canvas(grid);
+        move_grid(grid);
+        usleep(200000);
+    }
 
-        free(grid);
-    } 
-    printf("     %d", HEIGHT*WIDTH);
     return 0;
 }
 
-int **init_matrix(int n, int m) {
-    int **matrix = malloc(n * sizeof(int *));
-    for (int i = 0; i < m; i++) {
-        matrix[i] = malloc(m * sizeof(int));
-    }
 
-    return matrix;
-}
-
-void output_matrix(int **matrix) {
-    int amount = 0;
+void init_grid_from_file(int grid[][WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            printf("%d", matrix[i][j]);
-            amount++;
-
-            if (j != WIDTH) printf(" ");
+            int num;
+            scanf("%d", &num);
+            grid[i][j] = num;
         }
-
-        printf("\n");
     }
-
-    printf("AMOUNT: %d", amount);
 }
 
-
-void init_grid_from_file(int **grid) {
+void render_canvas(int grid[][WIDTH]) {
+    clear();    
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            char ch;
-            scanf("%c", &ch);
-            if (ch == EOF) break; // replace break with smth else
-            if ((int)ch != 10) grid[i][j] = (ch == '1') ? 1 : 0;
-            // printf("%d ", ch);
+            if (grid[i][j])
+                printw("%c", 'o');
+            else
+                printw("%c", '-');
+        }
+        printw("\n");
+    }
+    refresh();
+}
+
+void move_grid(int grid[][WIDTH]) {
+    int grid_copy[HEIGHT][WIDTH];
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            int count = neighbours_count(grid, i, j);
+            if (count == 3) {
+                grid_copy[i][j] = 1;
+            } else if (count == 2)
+                grid_copy[i][j] = grid[i][j];
+            else
+                grid_copy[i][j] = 0;
         }
     }
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            grid[i][j] = grid_copy[i][j];
+        }
+    } 
+}
+
+int neighbours_count(int grid[][WIDTH], int i_el, int j_el) {
+    int count = 0;
+    int pos_j1 = j_el - 1, pos_j2 = (j_el + 1) % WIDTH;
+
+    if (pos_j1 < 0)
+        pos_j1 = WIDTH + pos_j1;
+
+    for (int i = i_el - 1; i <= i_el + 1; i++) {
+        int pos_i = i % HEIGHT;
+        if (pos_i < 0)
+            pos_i = HEIGHT + pos_i;
+
+        count += grid[pos_i][pos_j1] + grid[pos_i][pos_j2];
+    }
+
+    count += grid[(i_el + 1) % HEIGHT][j_el];
+
+    int pos_i = i_el - 1;
+    if (pos_i < 0)
+        pos_i = HEIGHT + pos_i;
+    count += grid[pos_i][j_el];
+
+    return count;
 }
