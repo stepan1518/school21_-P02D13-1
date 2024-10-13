66d1d2e9b146bfb7c201b3b53b7a290c026dc910
Butterwell Deathstroke <butterwd@ga-h6.msk.21-school.ru>
2024-10-13 17:05:58 +0300

    added interface

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -6,54 +7,145 @@
 #define WIDTH 80
 #define SPEED 300001
 
-void init_grid_from_file(int grid[][WIDTH]);
+int init_grid_from_file(int grid[][WIDTH]);
 void render_canvas(int grid[][WIDTH]);
 int move_grid(int grid[][WIDTH]);
 int neighbours_count(int grid[][WIDTH], int i_el, int j_el);
 void change_speed(int *speed, char action);
+void print_start_interface();
+void print_end_interface(int flag);
+int game(int grid[][WIDTH], int *speed);
 
 int main() {
     initscr();
-    nodelay(stdscr, TRUE);
     noecho();
+    raw();
+    curs_set(0);
 
     int speed = SPEED;
     int grid[HEIGHT][WIDTH];
 
-    init_grid_from_file(grid);
+    int error = 0;
+
+    error = init_grid_from_file(grid);
+    if (error == 1) {
+        printf("an error occured while opening a terminal\n");
+        return 1;
+    }
+
+    char action;
+    while (1) {
+        print_start_interface();
+        action = getch();
+        if (action == 'Q' || action == 'q' || action == 'S' || action == 's') break;
+    }
+
+    if (action == 'S' || action == 's') {
+        int end;
+        end = game(grid, &speed);
+        nodelay(stdscr, FALSE);
+        print_end_interface(end);
+        getch();
+    }
+
+    endwin();
+
+    return 0;
+}
+
+void print_start_interface() {
+    int start_row = HEIGHT / 3;
+    int start_col = WIDTH / 5;
+
+    mvprintw(start_row++, start_col, " GGG   AAAAA  M     M  EEEEE");
+    mvprintw(start_row++, start_col, "G      A   A  MM   MM  E    ");
+    mvprintw(start_row++, start_col, "G  GG  AAAAA  M M M M  EEEE ");
+    mvprintw(start_row++, start_col, "G   G  A   A  M  M  M  E    ");
+    mvprintw(start_row++, start_col, " GGG   A   A  M     M  EEEEE");
+
+    start_col = WIDTH / 10;
+    mvprintw(start_row++, start_col, " OOO   FFFFF    L       III   FFFFF  EEEEE");
+    mvprintw(start_row++, start_col, "O   O  F        L        I    F      E    ");
+    mvprintw(start_row++, start_col, "O   O  FFFF     L        I    FFFF   EEEE ");
+    mvprintw(start_row++, start_col, "O   O  F        L        I    F      E    ");
+    mvprintw(start_row++, start_col, " OOO   F        LLLLL   III   F      EEEEE");
+
+    start_row += 4;
+    mvprintw(start_row, start_col, "START (s)");
+    start_col += 33;
+    mvprintw(start_row, start_col, "QUIT (q)");
+
+    refresh();
+}
+
+void print_end_interface(int end_reason) {
+    clear();
+    int start_row = HEIGHT / 3;
+    int start_col = WIDTH / 5;
+
+    mvprintw(start_row++, start_col, " GGG   AAAAA  M     M  EEEEE");
+    mvprintw(start_row++, start_col, "G      A   A  MM   MM  E    ");
+    mvprintw(start_row++, start_col, "G  GG  AAAAA  M M M M  EEEE ");
+    mvprintw(start_row++, start_col, "G   G  A   A  M  M  M  E    ");
+    mvprintw(start_row++, start_col, " GGG   A   A  M     M  EEEEE");
+
+    mvprintw(start_row++, start_col, " OOO  V       V  EEEEE  RRRR");
+    mvprintw(start_row++, start_col, "O   O  V     V   E      R  R");
+    mvprintw(start_row++, start_col, "O   O   V   V    EEEE   RRRR");
+    mvprintw(start_row++, start_col, "O   O    V V     E      R R ");
+    mvprintw(start_row++, start_col, " OOO      V      EEEEE  R  R");
+
+    start_row += 4;
+
+    if (end_reason == -1) {
+        mvprintw(start_row, start_col, "YOU ENDED THE GAME");
+    } else if (end_reason == 2) {
+        mvprintw(start_row, start_col, "THE POPULATION WILL NOT DEVELOP ANYMORE");
+    } else {
+        mvprintw(start_row, start_col, "THE POPULATION DIED");
+    }
+
+    refresh();
+}
+
+int game(int grid[][WIDTH], int *speed) {
+    nodelay(stdscr, TRUE);
+
+    int end_reason;
 
     while (1) {
         render_canvas(grid);
 
         char action = getch();
-        if (action != ERR) {
-            change_speed(&speed, action);
-        }
+        if (action != ERR) change_speed(speed, action);
 
         int flag = move_grid(grid);
-        if (flag == 0 || action == 'q') {
+        if (flag != 0 || action == 'q') {
+            end_reason = flag != 0 ? flag : -1;
+            usleep(SPEED * 5);
             break;
         }
 
-        usleep(speed);
+        usleep(*speed);
     }
 
-    endwin();
-    return 0;
+    return end_reason;
 }
 
-void init_grid_from_file(int grid[][WIDTH]) {
+int init_grid_from_file(int grid[][WIDTH]) {
+    int error = 0;
+
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             int num;
-            scanw("%d", &num);
+            scanf("%d", &num);
             grid[i][j] = num;
         }
     }
 
-    if (!freopen("/dev/tty", "rw", stdin)) {
-        exit(1);
-    }
+    if (!freopen("/dev/tty", "rw", stdin)) error = 1;
+
+    return error;
 }
 
 void render_canvas(int grid[][WIDTH]) {
@@ -70,6 +162,8 @@ void render_canvas(int grid[][WIDTH]) {
         printw("\n");
     }
 
+    printw("%s - slower\n%s - faster\n%s - quit\n", "z", "x", "q");
+
     refresh();
 }
 
@@ -79,7 +173,7 @@ int move_grid(int grid[][WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             int count = neighbours_count(grid, i, j);
-            
+
             if (count == 3) {
                 grid_copy[i][j] = 1;
             } else if (count == 2)
@@ -89,16 +183,16 @@ int move_grid(int grid[][WIDTH]) {
         }
     }
 
-    int count = 0, flag = 0;
+    int count = 0, flag = 2;
 
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             if (grid_copy[i][j] == 1) count++;
-            if (grid[i][j] != grid_copy[i][j]) flag = 1;
+            if (grid[i][j] != grid_copy[i][j]) flag = 0;
         }
     }
 
-    if (count == 0) flag = 0;
+    if (count == 0) flag = 1;
 
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
